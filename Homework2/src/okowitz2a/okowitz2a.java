4cf5fdd8309fea06233da75fdba089951e65c97a
tomokowitz <tomokowitz@gmail.com>
2017-02-02 15:31:53 -0500

    csc 516 hw2 2b add

--- Homework2/src/okowitz2a/okowitz2a.java
+++ Homework2/src/okowitz2a/okowitz2a.java
@@ -1,10 +1,17 @@
 package okowitz2a;
 
+import java.util.TreeSet;
+
 public class okowitz2a {
     public okowitz2a() {
     }
 
     public static void main(String[] args) {
         okowitz2a okowitz2a = new okowitz2a();
+        
+        
+        TreeSet customers = new TreeSet();
+        
+        
     }
 }
